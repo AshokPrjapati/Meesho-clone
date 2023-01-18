85af5a71b24144fe82f4125bbaf48bcea3a9b573
Prashant7970 <prashant887359@gmail.com>
2023-01-19 01:26:38 +0530

    upper nav

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import {ChakraProvider} from "@chakra-ui/react"
+
+import Navbar from '@/component/Navbar'
+import "../styles/globals.css"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider>
+  <Navbar/>
+ 
+  <Component {...pageProps} />
+  </ChakraProvider>
 }
