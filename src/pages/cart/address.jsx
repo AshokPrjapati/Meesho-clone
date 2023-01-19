1ed1a545fbbdf38d5342266cd83e8dc25f9214b5
Ashok Kumar <ap271449@gmail.com>
2023-01-19 09:52:07 +0530

    added : address page

--- src/pages/cart/address.jsx
+++ src/pages/cart/address.jsx
@@ -1,7 +1,21 @@
+import CartNav from "@/components/cart/CartNav";
+import Head from "next/head";
 import React from "react";
 
 const Address = () => {
-  return <div>A</div>;
+  return (
+    <>
+      <Head>
+        <title>ApniDukan-Address</title>
+        <meta name="description" content="Apni dukan Address page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <CartNav image={"../../../images/s2.png"} />
+      </div>
+    </>
+  );
 };
 
 export default Address;
