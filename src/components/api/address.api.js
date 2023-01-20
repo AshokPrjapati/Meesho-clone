1070b0417d65506136e62e567a625fbceaa3ecdd
Ashok Kumar <ap271449@gmail.com>
2023-01-20 11:06:19 +0530

    changed : syntax

--- src/components/api/address.api.js
+++ src/components/api/address.api.js
@@ -1,13 +1,17 @@
 const { default: axios } = require("axios");
 
-export const fetchAddress = () => {
-    return axios.get(`http://localhost:8080/address`).then((res => res.data)).catch(er => alert("something went wrong"));
+export const fetchAddress = async () => {
+    let res = await axios.get(`http://localhost:8080/address`);
+    let data = await res.data;
+    return data;
 };
 
-export const postAddress = (data) => {
-    return axios.post(`http://localhost:8080/address`, data).then((res => res.data)).catch(er => alert("something went wrong"));
+export const postAddress = async (data) => {
+    let res = await axios.post(`http://localhost:8080/address`, data);
+    return await res.data;
 };
 
-export const patchAddress = (data, id) => {
-    return axios.patch(`http://localhost:8080/address/${id}`, data).then((res => res.data)).catch(er => alert("something went wrong"));
+export const patchAddress = async (data, id) => {
+    let res = axios.patch(`http://localhost:8080/address/${id}`, data);
+    return await res.data;
 }
