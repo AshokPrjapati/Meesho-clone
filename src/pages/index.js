6c845499d1660c8a1398cd08d16de461402e3a4b
shivam malik <shivammalik962@gmail.com>
2023-01-20 14:24:51 +0530

    comiting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 
 import {  Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Header from "./components/Admin/Header"
-import LeftNavbar from './components/Admin/LeftNavbar'
-import Content from './components/Admin/Content'
+import Header from "../components/Admin/Header"
+import LeftNavbar from '../components/Admin/LeftNavbar'
+import Content from '../components/Admin/Content'
 
 const inter = Inter({ subsets: ['latin'] })
 
