55a5cf908c07e7d1a1565a9ebc09768509eb2d9e
Mehul-Kanjariya <mehulktik@gmail.com>
2023-01-22 22:56:55 +0530

    Filter updated

--- src/components/Admin/Update_Product/UpdateProduct.jsx
+++ src/components/Admin/Update_Product/UpdateProduct.jsx
@@ -76,10 +76,9 @@ const UpdateProduct = () => {
             >
               <option value="women-ethnic">Women Ethnic</option>
               <option value="women-western">Women Western</option>
-              <option value="men">Men</option>
-              <option value="kids">Kids</option>
+              <option value="Kids">Kids</option>
               <option value="home-kitchen">Home & Kitchen</option>
-              <option value="beauty-health">Beauty & Health</option>
+              <option value="beauty">Beauty & Health</option>
             </Select>
           </Box>
         </Flex>
