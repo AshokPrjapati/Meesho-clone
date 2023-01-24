980908ab169da149dd521822ac5d37f08f9211e3
Ashok Kumar <ap271449@gmail.com>
2023-01-24 12:23:23 +0530

    fixed : api

--- src/api.js
+++ src/api.js
@@ -1,2 +1,3 @@
 // export const api = " http://localhost:8080";
-export const api = "https://lazy-erin-caridea-veil.cyclic.app"
+// export const api = "https://lazy-erin-caridea-veil.cyclic.app"
+export const api = `https://apni-dukan.onrender.com`
